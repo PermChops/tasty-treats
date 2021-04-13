26b2407a523abf1f2744beafb2de2f3787877d7b
PermChops <82423411+PermChops@users.noreply.github.com>
2021-04-12 21:44:59 -0400

    Add files via upload

---
+++ cope.py
@@ -0,0 +1,52 @@
+import discord
+from pycoingecko import CoinGeckoAPI
+import asyncio
+
+token = 'cope'
+guildName = "COPE"
+botToken = 'your_bot_token'
+
+async def update(price,change):
+  print('updating')
+  try:
+    if(float(change) > 0):
+      stt = discord.Status.online
+      change = "+" + change
+    else:
+      stt = discord.Status.dnd
+    await client.change_presence(
+        status= stt,
+        activity=discord.Activity(type=discord.ActivityType.watching,
+                                  name=change+"%" + " | copeusd coingecko price | made by @vicyyn")
+    );
+    guild = [ guild for guild in client.guilds if guild.name== guildName]
+    await guild[0].me.edit(nick = price + " USD")
+  except Exception as e:
+    print(e)
+  print('updated')
+
+async def main():
+  while True:
+      try:
+        coin = cg.get_price(include_24hr_change='true',ids=token, vs_currencies='usd')
+        print(coin[token]['usd'])
+        price = str("{:.2f}".format(coin[token]['usd']))
+        change = str(coin[token]['usd_24h_change'])
+        change = "{:.2f}".format(float(change))
+        print("price is : " + price + " change : " + change)
+      except:
+  continue
+        await asyncio.sleep(10)
+      await update(price,change)
+      await asyncio.sleep(5)
+
+
+client = discord.Client()
+cg = CoinGeckoAPI()
+
+@client.event
+async def on_ready():
+    print('logged in as {0.user}'.format(client))
+    await main()
+
+client.run(botToken,  reconnect = True)
